020b36502e8d0551f06ecf425b0f22e283ef07f4
Fred Bunt <fredrick.bunt@gmail.com>
2019-09-26 13:53:11 -0600

    Refactor to handle batch conversion and group more efficiently

--- scripts/tb_to_netcdf.py
+++ scripts/tb_to_netcdf.py
@@ -4,6 +4,7 @@ import glob
 import netCDF4 as nc
 import numpy as np
 import os
+import pandas as pd
 import tqdm
 from collections import namedtuple
 from itertools import groupby
@@ -15,66 +16,74 @@ import tb as tbmod
 ROWS, COLS = eg.GRID_NAME_TO_SHAPE[eg.ML]
 
 
-def _parse_date(fname):
+def _parse_date(year, day_of_year):
+    dt_y = np.datetime64(year, dtype="datetime64[Y]")
+    date = dt_y + np.timedelta64(int(day_of_year) - 1, "D")
+    d = date.astype("O")
+    return dt.datetime(d.year, d.month, d.day)
+
+
+def _parse_date_from_fname(fname):
     m = tbmod.EASE_FNAME_PAT.match(os.path.basename(fname))
     if not m:
         return None
     _, _, y, doy, _, _, _ = m.groups()
-    dt_y = np.datetime64(y, dtype="datetime64[Y]")
-    date = dt_y + np.timedelta64(int(doy) - 1, "D")
-    d = date.astype("O")
-    return dt.datetime(d.year, d.month, d.day)
+    return _parse_date(y, doy)
 
 
 def _get_tb_meta(fname):
     m = tbmod.EASE_FNAME_PAT.match(os.path.basename(fname))
     if not m:
         return None
-    _, _, _, _, pass_type, freq, pol = m.groups()
-    return pass_type, freq, pol
+    # sat_id, proj, year, doy, pass_type, freq, pol
+    return m.groups()
 
 
-def tb_files_to_data(files, lon=None, lat=None):
+def load_data(files, proj):
     grids = np.empty((len(files), ROWS, COLS))
     for i, f in enumerate(files):
         grids[i] = tbmod.load_tb_file(f)
-    dates = np.array([_parse_date(f) for f in files])
-    if lon is None or lat is None:
-        lon, lat = eg.ease1_get_full_grid_lonlat(eg.ML)
-        x, y = eg.ease1_lonlat_to_meters(lon, lat)
-        lon = lon[0]
-        lat = lat[:, 0]
-        x = x[0]
-        y = y[:, 0]
-    pass_type, freq, pol = _get_tb_meta(files[0])
-    return dates, lon, lat, x, y, grids, pass_type, freq, pol
+    dates = np.array([_parse_date_from_fname(f) for f in files])
+    lon, lat = eg.ease1_get_full_grid_lonlat(proj)
+    x, y = eg.ease1_lonlat_to_meters(lon, lat, proj)
+    lon = lon[0]
+    lat = lat[:, 0]
+    x = x[0]
+    y = y[:, 0]
+    return dates, lon, lat, x, y, grids
 
 
 def build_tb_netcdf(
-    out_fname, dates, lon, lat, x, y, grids, pass_type, freq, pol
+    out_fname, dates, lon, lat, x, y, grids, sat_id, proj, pass_type, pol, freq
 ):
     ds = nc.Dataset(out_fname, "w")
     dtime = ds.createDimension("time", None)
     dlon = ds.createDimension("x", len(x))
     dlat = ds.createDimension("y", len(y))
+    # Time
     vtimes = ds.createVariable("time", "f8", ("time",))
     vtimes.calendar = "proleptic_gregorian"
     vtimes.units = f"days since {dates[0]}"
     vtimes[:] = nc.date2num(
         dates, vtimes.units, calendar="proleptic_gregorian"
     )
+    # x
     vx = ds.createVariable("x", "f8", ("x"))
     vx.units = "meters"
     vx[:] = x
+    # y
     vy = ds.createVariable("y", "f8", ("y"))
     vy.units = "meters"
     vy[:] = y
+    # lon
     vlon = ds.createVariable("lon", "f4", ("x",))
     vlon.units = "degrees east"
     vlon[:] = lon
+    # lat
     vlat = ds.createVariable("lat", "f4", ("y",))
     vlat.units = "degrees north"
     vlat[:] = lat
+    # tb
     vtb = ds.createVariable(
         "tb",
         "f4",
@@ -88,11 +97,12 @@ def build_tb_netcdf(
     vtb[:] = grids
     ds.description = f"Temperature Brightness data"
     ds.history = f"Created on {dt.datetime.now()} with python's netCDF4 module"
+    ds.satellite_id = sat_id
     ds.pass_type = pass_type
     ds.frequency = freq
     ds.polarization = pol
-    ds.grid_type = "EASE Grid 1.0 - Global (ML) 25 km"
-    ds.projection = "EPSG:3410"
+    ds.ease_grid_type = proj
+    ds.projection = eg.GRID_NAME_TO_PROJ_CODE[proj]
     ds.close()
 
 
@@ -106,48 +116,81 @@ def _find_all_data_files(root_dir):
 
 
 _FileGroup = namedtuple(
-    "_FileGroup", ("files", "year", "pass_type", "freq", "pol")
+    "_FileGroup",
+    ("files", "year", "sat_id", "proj", "pass_type", "pol", "freq"),
 )
-_TbFile = namedtuple(
-    "_TbFile", ("path", "date", "year", "pass_type", "freq", "pol")
-)
+_tb_fields = ("path", "year", "sat_id", "proj", "pass_type", "pol", "freq")
+_TbFile = namedtuple("_TbFile", _tb_fields)
 
 
 def _fname_to_meta_obj(path):
-    date = _parse_date(path)
-    return _TbFile(path, date, date.year, *_get_tb_meta(path))
+    y, doy, proj, sid, pt, pol, f = _get_tb_meta(path)
+    date = _parse_date(y, doy)
+    return _TbFile(path, date.year, sid, proj, pt, pol, f)
 
 
 def _group_files(all_files):
     files = [_fname_to_meta_obj(f) for f in all_files]
-    groups = {}
-    ptfunc = lambda x: x.pass_type
-    polfunc = lambda x: x.pol
-    ffunc = lambda x: x.freq
-    files.sort(key=lambda x: x.date)
-    for y, yg in groupby(files, lambda x: x.date.year):
-        yg = list(yg)
-        yg.sort(key=ptfunc)
-        for pt, ptg in groupby(yg, ptfunc):
-            ptg = list(ptg)
-            ptg.sort(key=polfunc)
-            for pol, polg in groupby(ptg, polfunc):
-                polg = list(polg)
-                polg.sort(key=ffunc)
-                for f, fg in groupby(polg, ffunc):
-                    if y not in groups:
-                        groups[y] = {}
-                    if pt not in groups[y]:
-                        groups[y][pt] = {}
-                    if pol not in groups[y][pt]:
-                        groups[y][pt][pol] = {}
-                    groups[y][pt][pol][f] = sorted(
-                        [x.path for x in fg], key=lambda x: x.path
-                    )
+    df = pd.DataFrame(files, columns=_tb_fields)
+    # list of the form:
+    # [((1987, 'F13', 'proj', 'A', 'H', '19'), DataFrame(matching rows)), ...]
+    df_groups = list(df.groupby(_tb_fields[1:]))
+    fgroups = []
+    for meta, gdf in df_groups:
+        paths = gdf["path"].to_list()
+        paths.sort()
+        fgroups.append(_FileGroup(paths, *meta))
+    return fgroups
+
+
+_ofname_fmt = "tb_{year}_{sat_id}_{proj}_{pass_type}_{freq}{pol}.nc"
+
+
+def _get_outfile_name(fg):
+    return _ofname_fmt.format(
+        year=fg.year,
+        sat_id=fg.sat_id,
+        proj=fg.proj,
+        pass_type=fg.pass_type,
+        freq=fg.freq,
+        pol=fg.pol,
+    )
+
+
+def _handle_group(fg, out_dir, overwrite):
+    group_dir = os.path.join(out_dir, fg.year)
+    if not os.path.isdir(group_dir):
+        os.makedirs(group_dir)
+    outpath = os.path.join(group_dir, _get_outfile_name(fg))
+    if not overwrite and os.path.isfile(outpath):
+        print(f"File already present: '{outpath}'")
+        return
+    build_tb_netcdf(
+        outpath,
+        *load_data(fg.files, fg.proj),
+        fg.sat_id,
+        fg.proj,
+        fg.pass_type,
+        fg.pol,
+        fg.freq,
+    )
+
+
+def batch_flat_to_netcdf(root_dir, out_dir, overwrite=False):
+    files = _find_all_data_files(root_dir)
+    fgroups = _group_files(files)
+    for fg in fgroups:
+        _handle_group(fg, out_dir, overwrite)
 
 
 def _validate_file_path(path):
     if not os.path.isfile(path):
+        raise IOError(f"Could not locate path: '{path}'")
+    return path
+
+
+def _validate_dir_path(path):
+    if not os.path.isdir(path):
         raise IOError(f"Could not locate path: '{path}'")
     return path
 
@@ -157,19 +200,15 @@ def _get_parser():
     p.add_argument(
         "-O", "--overwrite", action="store_true", help="Overwrite output file"
     )
-    p.add_argument("outfile", help="output file path")
-    p.add_argument(
-        "files",
-        nargs="+",
-        type=_validate_file_path,
-        help="The files to be processed",
+    p.add_argumetn(
+        "input_dir",
+        type=_validate_dir_path,
+        help="Directory to look for files in",
     )
+    p.add_argument("out_dir", help="output directory path")
     return p
 
 
 if __name__ == "__main__":
     args = _get_parser().parse_args()
-    files = sorted(args.files)
-    if os.path.isfile(args.outfile) and not args.overwrite:
-        raise IOError("Output file already present")
-    build_tb_netcdf(args.outfile, *tb_files_to_data(files))
+    batch_flat_to_netcdf(args.input_dir, args.out_dir, args.overwrite)
