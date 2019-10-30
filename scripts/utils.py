1b2bb8f35af7688af0bb4b128d0afb1a736a73a4
Fred Bunt <fredrick.bunt@gmail.com>
2019-10-30 15:03:09 -0600

    Add file and dir validation funcs

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import numpy as np
+import os
 
 
 def day_of_year_to_datetime(year, day_of_year):
@@ -13,3 +14,15 @@ def day_of_year_to_date(year, day_of_year):
     dt_y = np.datetime64(year, dtype="datetime64[Y]")
     date = dt_y + np.timedelta64(int(day_of_year) - 1, "D")
     return date.astype("O")
+
+
+def validate_file_path(path):
+    if os.path.isfile(path):
+        return path
+    raise IOError(f"Could not find file: '{path}'")
+
+
+def validate_dir_path(path):
+    if os.path.isdir(path):
+        return path
+    raise IOError(f"Could not find dir: '{path}'")
