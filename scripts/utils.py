0dd79fbdd0f088ed141b2e069cd3c33de533ffea
Fred Bunt <fredrick.bunt@gmail.com>
2019-12-13 16:36:31 -0700

    Reuse code in utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,17 +3,15 @@ import numpy as np
 import os
 
 
-def day_of_year_to_datetime(year, day_of_year):
-    dt_y = np.datetime64(year, dtype="datetime64[Y]")
-    date = dt_y + np.timedelta64(int(day_of_year) - 1, "D")
-    d = date.astype("O")
-    return dt.datetime(d.year, d.month, d.day)
-
-
 def day_of_year_to_date(year, day_of_year):
     dt_y = np.datetime64(year, dtype="datetime64[Y]")
     date = dt_y + np.timedelta64(int(day_of_year) - 1, "D")
     return date.astype("O")
+
+
+def day_of_year_to_datetime(year_str, day_of_year_str):
+    d = day_of_year_to_date(year_str, day_of_year_str)
+    return dt.datetime(d.year, d.month, d.day)
 
 
 def validate_file_path(path):
