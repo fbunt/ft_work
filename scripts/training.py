3606345729b3c994438bea8b3a8c908e3ad61f29
Fred Bunt <fredrick.bunt@gmailc.om>
2020-04-07 20:07:43 -0600

    Refactor to use new dataset classes and use ERA as label for now

--- scripts/training.py
+++ scripts/training.py
@@ -1,52 +1,60 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import torch
 import torch.nn as nn
 import tqdm
 
 from dataloading import (
-    AWSFuzzyLabelDataset,
-    DatabaseReference,
-    FTDataset,
-    KEY_INPUT_DATA,
-    KEY_LABEL_DATA,
+    # AWSFuzzyLabelDataset,
+    # DatabaseReference,
+    ComposedDictDataset,
+    KEY_ERA5_LABEL,
+    KEY_TB_DATA,
     NCTbDataset,
+    NCTbDatasetKeyedWrapper,
+    NpyDataset,
     ViewCopyTransform,
 )
-from model import UNet, local_variation_loss, ft_loss, LABEL_OTHER
-from validation_db_orm import get_db_session
+from model import UNet, local_variation_loss
+# from validation_db_orm import get_db_session
 
 
 in_chan = 5
 nclasses = 3
 depth = 4
 base_filters = 32
-epochs = 1
-batch_size = 8
-learning_rate = 0.01
+epochs = 30
+batch_size = 10
+learning_rate = 0.0005
 learning_momentum = 0.9
-l2_reg_weight = 1
-lv_reg_weight = 1
+l2_reg_weight = 0.1
+lv_reg_weight = 0.1
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-transform = ViewCopyTransform(15, 62, 12, 191)
 
+transform = ViewCopyTransform(15, 62, 12, 191)
 base_water_mask = np.load("../data/masks/ft_esdr_water_mask.npy")
 water_mask = torch.tensor(transform(base_water_mask))
-root_data_dir = "../data/training/train"
 
-db = DatabaseReference("../data/dbs/wmo_gsod.db", get_db_session)
+root_data_dir = "../data/train/"
+tb_ds = NCTbDatasetKeyedWrapper(
+    NCTbDataset(root_data_dir, transform=transform)
+)
+era_ds = NpyDataset(
+    KEY_ERA5_LABEL,
+    os.path.join(root_data_dir, "era5_t2m/era5-t2m-bidaily-2009-ak.npy"),
+)
+ds = ComposedDictDataset([tb_ds, era_ds])
+dataloader = torch.utils.data.DataLoader(
+    ds, batch_size=batch_size, shuffle=True, drop_last=True
+)
+
+# db = DatabaseReference("../data/dbs/wmo_gsod.db", get_db_session)
 model = UNet(
     in_chan, nclasses, depth=depth, base_filter_bank_size=base_filters
 )
 model.to(device)
-dataset = FTDataset(
-    NCTbDataset(root_data_dir, transform=transform),
-    AWSFuzzyLabelDataset(db, transform=transform, other_mask=base_water_mask),
-)
-dataloader = torch.utils.data.DataLoader(
-    dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=4
-)
 # opt = torch.optim.SGD(
 opt = torch.optim.Adam(
     model.parameters(),
@@ -59,7 +67,6 @@ loss_vec = []
 
 criterion = nn.CrossEntropyLoss()
 iters = 0
-print_period = 50
 for epoch in range(epochs):
     it = tqdm.tqdm(
         enumerate(dataloader),
@@ -68,22 +75,26 @@ for epoch in range(epochs):
         desc=f"Epoch: {epoch + 1}/{epochs}",
     )
     for i, data in it:
-        input_data = data[KEY_INPUT_DATA]
-        label = data[KEY_LABEL_DATA]
-        it.write(f"{label.shape}")
-        input_data = input_data.to(device, dtype=torch.float)
+        input_data = data[KEY_TB_DATA].to(device, dtype=torch.float)
+        # Compress 1-hot encoding to single channel
+        label = data[KEY_ERA5_LABEL].argmax(dim=1).to(device)
 
         model.zero_grad()
         output = model(input_data)
-        label = label.to(device)
-        loss = criterion(output, label) + (
-            lv_reg_weight * local_variation_loss(torch.sigmoid(output))
+        loss = criterion(output, label) + lv_reg_weight * local_variation_loss(
+            torch.sigmoid(output)
         )
         loss.backward()
         opt.step()
         loss_vec.append(loss.item())
         it.write(f"{loss.item()}")
         iters += 1
+    if epoch > 20:
+        for g in opt.param_groups:
+            g["lr"] = learning_rate / 100.0
+    elif epoch > 10:
+        for g in opt.param_groups:
+            g["lr"] = learning_rate / 10.0
 fmt = "../models/unet-in_{}-nclass_{}-depth_{}-{}.pt"
 torch.save(
     model.state_dict(),
