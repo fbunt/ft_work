4342b6ace6d8a2f26cc5ae0f26aac7b6d13905cc
Fred Bunt <fredrick.bunt@gmailc.om>
2020-05-04 17:33:45 -0600

    Update to process more data, use tqdm, and output masks

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -2,6 +2,7 @@ import datetime as dt
 import glob
 import numpy as np
 import torch
+import tqdm
 
 import dataloading as dl
 
@@ -10,6 +11,10 @@ transform = dl.ViewCopyTransform(15, 62, 12, 191)
 base_water_mask = np.load("../data/masks/ft_esdr_water_mask.npy")
 
 # Training data
+tb_files = sorted(glob.glob("../data/tb/2007/tb_2007_F17_ML_D*.nc"))
+tds07 = dl.GridsStackDataset(
+    [dl.NCDataset([f], "tb", transform=transform) for f in tb_files]
+)
 tb_files = sorted(glob.glob("../data/tb/2008/tb_2008_F17_ML_D*.nc"))
 tds08 = dl.GridsStackDataset(
     [dl.NCDataset([f], "tb", transform=transform) for f in tb_files]
@@ -18,12 +23,18 @@ tb_files = sorted(glob.glob("../data/tb/2009/tb_2009_F17_ML_D*.nc"))
 tds09 = dl.GridsStackDataset(
     [dl.NCDataset([f], "tb", transform=transform) for f in tb_files]
 )
-tb_ds = torch.utils.data.ConcatDataset([tds08, tds09])
+tb_files = sorted(glob.glob("../data/tb/2010/tb_2010_F17_ML_D*.nc"))
+tds10 = dl.GridsStackDataset(
+    [dl.NCDataset([f], "tb", transform=transform) for f in tb_files]
+)
+tb_ds = torch.utils.data.ConcatDataset([tds07, tds08, tds09, tds10])
 
 era_ds = dl.ERA5BidailyDataset(
     [
+        "../data/era5/t2m/bidaily/era5-t2m-bidaily-2007.nc",
         "../data/era5/t2m/bidaily/era5-t2m-bidaily-2008.nc",
         "../data/era5/t2m/bidaily/era5-t2m-bidaily-2009.nc",
+        "../data/era5/t2m/bidaily/era5-t2m-bidaily-2010.nc",
     ],
     "t2m",
     "AM",
@@ -33,32 +44,40 @@ era_ds = dl.ERA5BidailyDataset(
 
 tb = []
 era = []
-d = dt.date(2008, 1, 1)
+vmask = []
+d = dt.date(2007, 1, 1)
 delta = dt.timedelta(days=1)
 dates = []
 dropped_dates = []
 idx = []
-for i, (tbi, erai) in enumerate(zip(tb_ds, era_ds)):
-    if not np.isnan(tbi).all():
-        tbi = tbi.numpy()
-        tbi[np.isnan(tbi)] = 0
+it = tqdm.tqdm(enumerate(zip(tb_ds, era_ds)), ncols=80, total=len(era_ds))
+for i, (tbi, erai) in it:
+    tbi = tbi.numpy()
+    nan = np.isnan(tbi)
+    if not nan.all():
+        for chan, na in zip(tbi, nan):
+            chan[na] = np.nanmean(chan)
         tb.append(tbi)
         era.append(erai)
+        m = (~nan)[0]
+        vmask.append(m)
         dates.append(d)
         idx.append(i)
     else:
-        print(f"Dropping: {d}")
+        it.write(f"Dropping: {d}")
         dropped_dates.append(d)
     d += delta
 tb = np.stack(tb, axis=0)
 era = np.stack(era, axis=0)
+vmask = np.stack(vmask, axis=0)
 
-np.save("../data/cleaned/tb-2008-2009-D-ak.npy", tb)
-np.save("../data/cleaned/era5-t2m-am-2008-2009-ak.npy", era)
-with open("../data/cleaned/date_map-2008-2009.csv", "w") as fd:
+np.save("../data/cleaned/tb-2007-2010-D-ak.npy", tb)
+np.save("../data/cleaned/era5-t2m-am-2007-2010-ak.npy", era)
+np.save("../data/cleaned/tb_valid_mask-2007-2010-D-ak.npy", vmask)
+with open("../data/cleaned/date_map-2007-2010.csv", "w") as fd:
     for i, d in zip(idx, dates):
         fd.write(f"{i},{d}\n")
-with open("../data/cleaned/dropped_dates-2008-2009.csv", "w") as fd:
+with open("../data/cleaned/dropped_dates-2007-2010.csv", "w") as fd:
     for d in dropped_dates:
         fd.write(f"{d}\n")
 tb = None
@@ -73,9 +92,7 @@ tds15 = dl.GridsStackDataset(
     [dl.NCDataset([f], "tb", transform=transform) for f in tb_files]
 )
 era_ds = dl.ERA5BidailyDataset(
-    [
-        "../data/era5/t2m/bidaily/era5-t2m-bidaily-2015.nc",
-    ],
+    ["../data/era5/t2m/bidaily/era5-t2m-bidaily-2015.nc"],
     "t2m",
     "AM",
     other_mask=base_water_mask,
@@ -84,28 +101,36 @@ era_ds = dl.ERA5BidailyDataset(
 
 tb = []
 era = []
+vmask = []
 d = dt.date(2015, 1, 1)
 delta = dt.timedelta(days=1)
 dates = []
 dropped_dates = []
 idx = []
-for i, (tbi, erai) in enumerate(zip(tds15, era_ds)):
-    if not np.isnan(tbi).all():
-        tbi = tbi.numpy()
-        tbi[np.isnan(tbi)] = 0
+it = tqdm.tqdm(enumerate(zip(tds15, era_ds)), ncols=80, total=len(era_ds))
+for i, (tbi, erai) in it:
+    tbi = tbi.numpy()
+    nan = np.isnan(tbi)
+    if not nan.all():
+        for chan, na in zip(tbi, nan):
+            chan[na] = np.nanmean(chan)
         tb.append(tbi)
         era.append(erai)
+        m = (~nan)[0]
+        vmask.append(m)
         dates.append(d)
         idx.append(i)
     else:
-        print(f"Dropping: {d}")
+        it.write(f"Dropping: {d}")
         dropped_dates.append(d)
     d += delta
 tb = np.stack(tb, axis=0)
 era = np.stack(era, axis=0)
+vmask = np.stack(vmask, axis=0)
 
 np.save("../data/cleaned/tb-2015-D-ak.npy", tb)
 np.save("../data/cleaned/era5-t2m-am-2015-ak.npy", era)
+np.save("../data/cleaned/tb_valid_mask-2015-D-ak.npy", vmask)
 with open("../data/cleaned/date_map-2015.csv", "w") as fd:
     for i, d in zip(idx, dates):
         fd.write(f"{i},{d}\n")
