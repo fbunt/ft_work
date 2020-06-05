2da1bd20e9731000f27c28ff6fe5b75fd6ddc40d
Fred Bunt <fredrick.bunt@gmailc.om>
2020-06-05 15:04:41 -0600

    Add nh transform

--- scripts/transforms.py
+++ scripts/transforms.py
@@ -1,4 +1,7 @@
 from dataloading import ViewCopyTransform
 
 
+# Copies a small window around Alaska
 AK_VIEW_TRANS = ViewCopyTransform(15, 62, 12, 191)
+# Copies the northern hemisphere
+NH_VIEW_TRANS = ViewCopyTransform(0, 292, 0, 1383)
