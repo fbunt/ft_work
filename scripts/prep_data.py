f06a3a394bb14c694059f017ea92360f6d9d2e8a
Fred Bunt <fredrick.bunt@gmailc.om>
2020-06-15 18:39:46 -0600

    Refactor for temporal interpolation and using arrays over datasets

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -22,41 +22,63 @@ def build_tb_ds(path_groups, transform):
     return torch.utils.data.ConcatDataset(dss)
 
 
-def prep_data(start_date, solar_ds, tb_ds, era_ds, out_dir, region):
-    out_dir = os.path.abspath(out_dir)
-    solar = []
-    tb = []
-    era = []
-    vmask = []
+def dataset_to_array(ds):
+    n = len(ds)
+    shape = (n, *ds[0].shape)
+    ar = np.zeros(shape)
+    for i, x in enumerate(ds):
+        ar[i] = x
+    return ar
+
+
+def get_n_dates(start_date, n):
+    dates = []
     d = dt.date(start_date.year, start_date.month, start_date.day)
     delta = dt.timedelta(days=1)
-    dates = []
-    dropped_dates = []
-    idx = []
-    it = tqdm.tqdm(
-        enumerate(zip(solar_ds, tb_ds, era_ds)), ncols=80, total=len(era_ds)
-    )
-    for i, (sri, tbi, erai) in it:
-        tbi = tbi.numpy()
-        nan = np.isnan(tbi)
-        if not nan.all():
-            for chan, na in zip(tbi, nan):
-                chan[na] = np.nanmean(chan)
-            solar.append(sri)
-            tb.append(tbi)
-            era.append(erai)
-            m = (~nan)[0]
-            vmask.append(m)
-            dates.append(d)
-            idx.append(i)
-        else:
-            it.write(f"Dropping: {d}")
-            dropped_dates.append(d)
+    for i in range(n):
+        dates.append(d)
         d += delta
-    solar = np.stack(solar, axis=0)
-    tb = np.stack(tb, axis=0)
-    era = np.stack(era, axis=0)
-    vmask = np.stack(vmask, axis=0)
+    return dates
+
+
+def fill_gaps(x, nanmask):
+    cleaned = x.copy()
+    j = 0
+    while True:
+        for i in tqdm.tqdm(range(len(x)), ncols=80, desc=f"Gap Fill {j + 1}"):
+            m = nanmask[i]
+            if m.any():
+                left = i - 1
+                right = i + 1
+                if right == len(x):
+                    right = 0
+                cleaned[i][m] = np.nanmean([x[left][m], x[right][m]], axis=0)
+        nanmask = np.isnan(cleaned)
+        if not nanmask.any():
+            break
+        x = cleaned.copy()
+        j += 1
+    return cleaned
+
+
+def prep(start_date, solar, tb, era, out_dir, region):
+    out_dir = os.path.abspath(out_dir)
+    n = len(solar)
+    dates = np.array(get_n_dates(start_date, n))
+    # Filter out indices where all Tb data is missing
+    good_idxs = [i for i in range(n) if not np.isnan(tb[i]).all()]
+    bad_idxs = [i for i in range(n) if np.isnan(tb[i]).all()]
+    n = len(good_idxs)
+    dropped_dates = dates[bad_idxs]
+    dates = dates[good_idxs]
+    solar = solar[good_idxs]
+    tb = tb[good_idxs]
+    era = era[good_idxs]
+    nanmask = np.isnan(tb)
+    tb = fill_gaps(tb, nanmask)
+    # Only need one of the identical masks
+    nanmask = nanmask[:, 0]
+    vmask = nanmask
 
     start_year = dates[0].year
     end_year = dates[-1].year
@@ -70,7 +92,7 @@ def prep_data(start_date, solar_ds, tb_ds, era_ds, out_dir, region):
     np.save(f"{out_dir}/era5-t2m-am-{year_str}-{region}.npy", era)
     np.save(f"{out_dir}/tb_valid_mask-{year_str}-D-{region}.npy", vmask)
     with open(f"{out_dir}/date_map-{year_str}.csv", "w") as fd:
-        for i, d in zip(idx, dates):
+        for i, d in zip(good_idxs, dates):
             fd.write(f"{i},{d}\n")
     with open(f"{out_dir}/dropped_dates-{year_str}.csv", "w") as fd:
         for d in dropped_dates:
@@ -87,13 +109,15 @@ base_water_mask = np.load("../data/masks/ft_esdr_water_mask.npy")
 out_dir = "../data/cleaned"
 
 # Training data
-solar_ds = torch.utils.data.ConcatDataset(
-    [
-        dl.NpyDataset(f"../data/solar/solar_rad-daily-2007-{region}.npy"),
-        dl.NpyDataset(f"../data/solar/solar_rad-daily-2008-{region}.npy"),
-        dl.NpyDataset(f"../data/solar/solar_rad-daily-2009-{region}.npy"),
-        dl.NpyDataset(f"../data/solar/solar_rad-daily-2010-{region}.npy"),
-    ]
+solar = dataset_to_array(
+    torch.utils.data.ConcatDataset(
+        [
+            dl.NpyDataset(f"../data/solar/solar_rad-daily-2007-{region}.npy"),
+            dl.NpyDataset(f"../data/solar/solar_rad-daily-2008-{region}.npy"),
+            dl.NpyDataset(f"../data/solar/solar_rad-daily-2009-{region}.npy"),
+            dl.NpyDataset(f"../data/solar/solar_rad-daily-2010-{region}.npy"),
+        ]
+    )
 )
 path_groups = [
     glob.glob("../data/tb/2007/tb_2007_F17_ML_D*.nc"),
@@ -101,32 +125,38 @@ path_groups = [
     glob.glob("../data/tb/2009/tb_2009_F17_ML_D*.nc"),
     glob.glob("../data/tb/2010/tb_2010_F17_ML_D*.nc"),
 ]
-tb_ds = build_tb_ds(path_groups, transform)
-era_ds = dl.ERA5BidailyDataset(
-    [
-        "../data/era5/t2m/bidaily/era5-t2m-bidaily-2007.nc",
-        "../data/era5/t2m/bidaily/era5-t2m-bidaily-2008.nc",
-        "../data/era5/t2m/bidaily/era5-t2m-bidaily-2009.nc",
-        "../data/era5/t2m/bidaily/era5-t2m-bidaily-2010.nc",
-    ],
-    "t2m",
-    "AM",
-    other_mask=base_water_mask,
-    transform=transform,
+tb = dataset_to_array(build_tb_ds(path_groups, transform))
+era = dataset_to_array(
+    dl.ERA5BidailyDataset(
+        [
+            "../data/era5/t2m/bidaily/era5-t2m-bidaily-2007.nc",
+            "../data/era5/t2m/bidaily/era5-t2m-bidaily-2008.nc",
+            "../data/era5/t2m/bidaily/era5-t2m-bidaily-2009.nc",
+            "../data/era5/t2m/bidaily/era5-t2m-bidaily-2010.nc",
+        ],
+        "t2m",
+        "AM",
+        other_mask=base_water_mask,
+        transform=transform,
+    )
 )
-prep_data(dt.date(2007, 1, 1), solar_ds, tb_ds, era_ds, out_dir, region)
+prep(dt.date(2007, 1, 1), solar, tb, era, out_dir, region)
 
 
 # Validation data
-solar_ds = dl.NpyDataset("../data/solar/solar_rad-daily-2015-{region}.npy")
-tb_ds = build_tb_ds(
-    [glob.glob("../data/tb/2015/tb_2015_F17_ML_D*.nc")], transform
+solar = dataset_to_array(
+    dl.NpyDataset("../data/solar/solar_rad-daily-2015-{region}.npy")
 )
-era_ds = dl.ERA5BidailyDataset(
-    ["../data/era5/t2m/bidaily/era5-t2m-bidaily-2015.nc"],
-    "t2m",
-    "AM",
-    other_mask=base_water_mask,
-    transform=transform,
+tb = dataset_to_array(
+    build_tb_ds([glob.glob("../data/tb/2015/tb_2015_F17_ML_D*.nc")], transform)
 )
-prep_data(dt.date(2015, 1, 1), solar_ds, tb_ds, era_ds, out_dir)
+era = dataset_to_array(
+    dl.ERA5BidailyDataset(
+        ["../data/era5/t2m/bidaily/era5-t2m-bidaily-2015.nc"],
+        "t2m",
+        "AM",
+        other_mask=base_water_mask,
+        transform=transform,
+    )
+)
+prep(dt.date(2015, 1, 1), solar, tb, era, out_dir)
