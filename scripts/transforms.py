da009fb917b8c189e319abc710d8b97346b527e6
Fred Bunt <fredrick.bunt@gmailc.om>
2020-06-18 16:13:51 -0600

    Add N45W transform

--- scripts/transforms.py
+++ scripts/transforms.py
@@ -7,3 +7,5 @@ AK_VIEW_TRANS = ViewCopyTransform(15, 62, 12, 191)
 NH_VIEW_TRANS = ViewCopyTransform(0, 292, 0, 1383)
 # Copies everything at and above N 45
 N45_VIEW_TRANS = ViewCopyTransform(0, 84, 0, 1383)
+# Copies everything at and above N 45 and everything west of 0
+N45W_VIEW_TRANS = ViewCopyTransform(0, 84, 0, 691)
