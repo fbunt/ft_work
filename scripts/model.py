c94691f9176d92b00e3baf1b1e53ad7e7b7ffa87
fbunt <fredrick.bunt@gmail.com>
2020-11-09 14:59:02 -0700

    Add UNet implementations with skip connections around the conv blocks

--- scripts/model.py
+++ scripts/model.py
@@ -40,6 +40,29 @@ class _MultiConv(nn.Module):
         return x
 
 
+class _MultiConvSkip(nn.Module):
+    """A block of multiple conv blocks with a skip connection around it."""
+
+    def __init__(self, in_chan, out_chan, n=2):
+        super().__init__()
+        if n < 1:
+            raise ValueError(
+                "Expected number of convolution layers to be greater than 1 "
+                f" (got {n})"
+            )
+        self.layers = nn.ModuleList([_ConvLayer(in_chan, out_chan)])
+        for _ in range(n - 1):
+            self.layers.append(_ConvLayer(out_chan, out_chan))
+        self.activation = nn.LeakyReLU(LEAKY_SLOPE, inplace=True)
+
+    def forward(self, x):
+        xin = x
+        for m in self.layers:
+            x = m(x)
+        x += xin
+        return self.activation(x)
+
+
 class _DownSample(nn.Module):
     def __init__(self, size=2):
         super().__init__()
@@ -54,6 +77,17 @@ class _Down(nn.Module):
         super().__init__()
         self.model = nn.Sequential(
             _DownSample(), _MultiConv(in_chan, out_chan)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class _DownSkip(nn.Module):
+    def __init__(self, in_chan, out_chan):
+        super().__init__()
+        self.model = nn.Sequential(
+            _DownSample(), _MultiConvSkip(in_chan, out_chan)
         )
 
     def forward(self, x):
@@ -76,6 +110,23 @@ class _Up(nn.Module):
         super().__init__()
         self.upsample = _UpSample(in_chan)
         self.conv = _MultiConv(in_chan, out_chan)
+
+    def forward(self, lhs, bot):
+        upbot = self.upsample(bot)
+        dh = lhs.size(2) - upbot.size(2)
+        dw = lhs.size(3) - upbot.size(3)
+        upbot = F.pad(
+            upbot, (dw // 2, dw - (dw // 2), dh // 2, dh - (dh // 2))
+        )
+        x = torch.cat((lhs, upbot), dim=1)
+        return self.conv(x)
+
+
+class _UpSkip(nn.Module):
+    def __init__(self, in_chan, out_chan):
+        super().__init__()
+        self.upsample = _UpSample(in_chan)
+        self.conv = _MultiConvSkip(in_chan, out_chan)
 
     def forward(self, lhs, bot):
         upbot = self.upsample(bot)
@@ -129,6 +180,43 @@ class UNet(nn.Module):
         return self.out(x)
 
 
+class UNetSkip(nn.Module):
+    """A depth-generalized UNet implementation with skip connections around the
+    convolution blocks.
+
+    ref: https://arxiv.org/abs/1505.04597
+    """
+
+    def __init__(self, in_chan, n_classes, depth=4, base_filter_bank_size=16):
+        # Note: in the original paper, base_filter_bank_size is 64
+        super().__init__()
+
+        if depth < 0:
+            raise ValueError(f"UNet depth must be at least 0: {depth}")
+        nb = base_filter_bank_size
+        if nb <= 0:
+            raise ValueError(f"Filter bank size must be greater than 0: {nb}")
+
+        self.depth = depth
+        self.input = _MultiConv(in_chan, nb)
+        self.downs = nn.ModuleList()
+        for i in range(0, depth):
+            self.downs.append(_DownSkip((2 ** i) * nb, (2 ** (i + 1)) * nb))
+        self.ups = nn.ModuleList()
+        for i in range(depth, 0, -1):
+            self.ups.append(_UpSkip((2 ** i) * nb, (2 ** (i - 1)) * nb))
+        self.out = nn.Conv2d(nb, n_classes, kernel_size=1)
+
+    def forward(self, x):
+        xdowns = [self.input(x)]
+        for down in self.downs:
+            xdowns.append(down(xdowns[-1]))
+        x = xdowns[-1]
+        for xleft, up in zip(xdowns[:-1][::-1], self.ups):
+            x = up(xleft, x)
+        return self.out(x)
+
+
 class UNetDepth4(nn.Module):
     def __init__(self, in_chan, n_classes, base_filter_bank_size=16):
         super().__init__()
@@ -162,6 +250,40 @@ class UNetDepth4(nn.Module):
         x = self.ups[2](xdowns[1], x)
         x = self.ups[3](xdowns[0], x)
         return self.out(x)
+
+
+class UNetSkipDepth4(nn.Module):
+    def __init__(self, in_chan, n_classes, base_filter_bank_size=16):
+        super().__init__()
+
+        nb = base_filter_bank_size
+        if nb <= 0:
+            raise ValueError(f"Filter bank size must be greater than 0: {nb}")
+
+        self.input = _MultiConv(in_chan, nb)
+        self.downs = nn.ModuleList()
+        self.downs.append(_DownSkip(1 * nb, 2 * nb))
+        self.downs.append(_DownSkip(2 * nb, 4 * nb))
+        self.downs.append(_DownSkip(4 * nb, 8 * nb))
+        self.downs.append(_DownSkip(8 * nb, 16 * nb))
+        self.ups = nn.ModuleList()
+        self.ups.append(_UpSkip(16 * nb, 8 * nb))
+        self.ups.append(_UpSkip(8 * nb, 4 * nb))
+        self.ups.append(_UpSkip(4 * nb, 2 * nb))
+        self.ups.append(_UpSkip(2 * nb, 1 * nb))
+        self.out = nn.Conv2d(nb, n_classes, kernel_size=1)
+
+    def forward(self, x):
+        xdowns = [self.input(x)]
+        xdowns.append(self.downs[0](xdowns[-1]))
+        xdowns.append(self.downs[1](xdowns[-1]))
+        xdowns.append(self.downs[2](xdowns[-1]))
+        xdowns.append(self.downs[3](xdowns[-1]))
+        x = xdowns[4]
+        x = self.ups[0](xdowns[3], x)
+        x = self.ups[1](xdowns[2], x)
+        x = self.ups[2](xdowns[1], x)
+        x = self.ups[3](xdowns[0], x)
         return self.out(x)
 
 
