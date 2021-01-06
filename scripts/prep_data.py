3db912dbcf63b12a2f8be39c47c7d6e7a00029d9
Fred <fredrick.bunt@gmail.com>
2021-01-05 18:00:53 -0700

    Allow for non-periodic datasets
    Instead of just stopping at either end of the data, in the non-periodic case, a bias is added to the counts where wrapping occurred. This bias will cause those elements to be weighted significantly less when filling the gaps with means.

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -59,37 +59,73 @@ def get_missing_ratio(x):
     return np.isnan(x).sum() / x.size
 
 
-def get_predecessor(x, i, missing, missing_func=np.isnan):
+def get_predecessor(
+    x,
+    i,
+    missing,
+    missing_func=np.isnan,
+    periodic=True,
+    non_periodic_bias_val=20,
+):
     px = x[i].copy()
     count = np.zeros(px.shape, dtype=int)
     j = i - 1
+    add_bias = False
+    if j < 0 and not periodic:
+        # If j has gone past the end and the dataset is not periodic, add the
+        # bias on the next iteration.
+        add_bias = True
     while missing.any():
         px[missing] = x[j, missing]
-        count[missing] += 1
+        count[missing] += 1 + (add_bias * non_periodic_bias_val)
         missing = missing_func(px)
         j -= 1
+        # If j has gone past the end and the dataset is not periodic, add the
+        # bias on the next iteration. If not, then add_bias is set to false and
+        # no bias will be added.
+        add_bias = (j < 0) and (not periodic)
+        if j < 0:
+            j = len(x) - 1
     idx = count != 0
     return px[idx], count[idx]
 
 
-def get_successor(x, i, missing, missing_func=np.isnan):
+def get_successor(
+    x,
+    i,
+    missing,
+    missing_func=np.isnan,
+    periodic=True,
+    non_periodic_bias_val=20,
+):
     sx = x[i].copy()
     count = np.zeros(sx.shape, dtype=int)
     j = i + 1
+    add_bias = False
     if j >= len(x):
         j = 0
+        if not periodic:
+            # If j has gone past the end and the dataset is not periodic, add
+            # the bias on the next iteration.
+            add_bias = True
     while missing.any():
         sx[missing] = x[j, missing]
-        count[missing] += 1
+        count[missing] += 1 + (add_bias * non_periodic_bias_val)
         missing = missing_func(sx)
         j += 1
+        # If j has gone past the end and the dataset is not periodic, add the
+        # bias on the next iteration. If not, then add_bias is set to false and
+        # no bias will be added.
+        add_bias = (j >= len(x)) and (not periodic)
         if j >= len(x):
             j = 0
     idx = count != 0
     return sx[idx], count[idx]
 
 
-def fill_gaps(x, missing_func=np.isnan):
+def fill_gaps(
+    x, missing_func=np.isnan, periodic=True, non_periodic_bias_val=20
+):
     gap_filled = x.copy()
     for i in tqdm.tqdm(range(len(x)), ncols=80, desc="Gap fill"):
         gaps = missing_func(x[i])
@@ -97,9 +133,13 @@ def fill_gaps(x, missing_func=np.isnan):
             continue
         # count is how far the alg had to go to find a value
         # Get past value
-        pred, pcount = get_predecessor(x, i, gaps, missing_func)
+        pred, pcount = get_predecessor(
+            x, i, gaps, missing_func, periodic, non_periodic_bias_val
+        )
         # Get future value
-        succ, scount = get_successor(x, i, gaps, missing_func)
+        succ, scount = get_successor(
+            x, i, gaps, missing_func, periodic, non_periodic_bias_val
+        )
         # Weighted mean
         total = pcount + scount
         # The predecessor/successor with the higher count should be weighted
@@ -139,6 +179,8 @@ def prep(
     region,
     drop_bad_days,
     missing_cutoff=0.6,
+    periodic=True,
+    non_periodic_bias_val=20,
 ):
     out_dir = os.path.abspath(out_dir)
     n = len(solar)
@@ -163,8 +205,17 @@ def prep(
     tb = tb[good_idxs]
     era_ft = era_ft[good_idxs]
     era_t2m = era_t2m[good_idxs]
-    tb = fill_gaps(tb)
-    snow = np.round(fill_gaps(snow, missing_func=is_neg_one))
+    tb = fill_gaps(
+        tb, periodic=periodic, non_periodic_bias_val=non_periodic_bias_val
+    )
+    snow = np.round(
+        fill_gaps(
+            snow,
+            missing_func=is_neg_one,
+            periodic=periodic,
+            non_periodic_bias_val=non_periodic_bias_val,
+        )
+    )
 
     start_year = dates[0].year
     end_year = dates[-1].year
