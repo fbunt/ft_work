a8e450582a8707b2fdf6a28ad1a12928c87647e5
Fred Bunt <fredrick.bunt@gmail.com>
2021-01-19 15:35:05 -0700

    Update script name to match

--- dl_scripts/download_tb.sh
+++ dl_scripts/download_tb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Downloading SMMR"
-python tb_download.py "sidads.colorado.edu" "pub/DATASETS/nsidc0071_smmr_ease_grid_tbs/global" "$1"
+python download_tb.py "sidads.colorado.edu" "pub/DATASETS/nsidc0071_smmr_ease_grid_tbs/global" "$1"
 
 echo "Downloading SSMI"
-python tb_download.py "sidads.colorado.edu" "pub/DATASETS/nsidc0032_ease_grid_tbs/global" "$2"
+python download_tb.py "sidads.colorado.edu" "pub/DATASETS/nsidc0032_ease_grid_tbs/global" "$2"
