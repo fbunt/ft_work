9f57c8bac2deb2dc8e8ac1acbab50fe5c68f8cbf
Fred Bunt <fredrick.bunt@gmail.com>
2021-03-17 22:40:58 -0600

    Fix extra col number. No effect, just helps reasoning

--- scripts/transforms.py
+++ scripts/transforms.py
@@ -10,9 +10,9 @@ GL_VIEW_TRANS = gl_view
 # Copies a small window around Alaska
 AK_VIEW_TRANS = ViewCopyTransform(15, 62, 12, 191)
 # Copies the northern hemisphere
-NH_VIEW_TRANS = ViewCopyTransform(0, 292, 0, 1383)
+NH_VIEW_TRANS = ViewCopyTransform(0, 292, 0, 1382)
 # Copies everything at and above N 45
-N45_VIEW_TRANS = ViewCopyTransform(0, 84, 0, 1383)
+N45_VIEW_TRANS = ViewCopyTransform(0, 84, 0, 1382)
 # Copies everything at and above N 45 and everything west of 0
 N45W_VIEW_TRANS = ViewCopyTransform(0, 84, 0, 691)
 
